d611634bc0201f20cc44708991d6554b0ab66bad
vikas010203 <vikashsahu8236@gmail.com>
2024-09-27 15:38:41 +0530

    day2

---
+++ project-1/src/Props/Child_1.jsx
@@ -0,0 +1,17 @@
+import Child_2 from "./Child_2";
+
+function Child_1(){
+
+let name = "vikash"
+
+    return(
+       <>
+
+        <h1> hello vikash</h1>
+        <h2> from mauganj</h2>
+        <Child_2 nam={ name}/>
+       </>
+    )
+}
+
+export default Child_1;
---
+++ project-1/src/Props/Child_2.jsx
@@ -0,0 +1,23 @@
+
+
+import React from 'react'
+
+
+
+const Child_2 = ({nam}) => {
+
+  return (
+
+    <div>
+       <h1>Hello123 {nam}  </h1>
+  
+
+    </div>
+    
+
+    
+  )
+
+}
+
+export default Child_2;
